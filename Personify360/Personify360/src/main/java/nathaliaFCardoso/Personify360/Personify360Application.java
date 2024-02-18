8d562e27e984733bba41c19cf8570fa28755b4ba
nathaliafcardoso <nathaliaf.cardoso@outlook.com>
2024-02-18 16:27:13 -0300

    first commit

---
+++ Personify360/Personify360/src/main/java/nathaliaFCardoso/Personify360/Personify360Application.java
@@ -0,0 +1,13 @@
+package nathaliaFCardoso.Personify360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Personify360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Personify360Application.class, args);
+	}
+
+}
---
+++ Personify360/Personify360/src/test/java/nathaliaFCardoso/Personify360/Personify360ApplicationTests.java
@@ -0,0 +1,13 @@
+package nathaliaFCardoso.Personify360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Personify360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
